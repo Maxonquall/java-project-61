304d9497bdd4a0162ed56d5f22b3728f46eab7dc
Maxim <msirotkin95@gmail.com>
2023-10-18 23:10:55 +0300

    codeclimate fixed

--- app/src/main/java/hexlet/games/Calc.java
+++ app/src/main/java/hexlet/games/Calc.java
@@ -6,12 +6,25 @@ import java.util.Random;
 public class Calc {
     public static String whatIsResult() {
         System.out.println("What is the result of the expression?");
-        var numberOne = generateNumber();
-        var numberTwo = generateNumber();
         var operation = generateOperation();
+        return calculation(operation);
+    }
+    private static int generateNumber() {
+        Random random = new Random();
+        final int numberOneRandomLimit = 100;
+        return random.nextInt(numberOneRandomLimit);
+    }
+    private static int generateOperation() {
+        Random random = new Random();
+        final int operationTypes = 3;
+        return random.nextInt(operationTypes);
+    }
+    private static String calculation(int operation) {
         var symbol = "";
         var result = 0;
         var resultToString = "";
+        var numberOne = generateNumber();
+        var numberTwo = generateNumber();
         switch (operation) {
             case 0:
                 symbol = "+";
@@ -34,14 +47,5 @@ public class Calc {
         System.out.println("Question: " + numberOne + " " + symbol + " " + numberTwo);
         return resultToString;
     }
-    private static int generateNumber() {
-        Random random = new Random();
-        final int numberOneRandomLimit = 100;
-        return random.nextInt(numberOneRandomLimit);
-    }
-    private static int generateOperation() {
-        Random random = new Random();
-        final int operationTypes = 3;
-        return random.nextInt(operationTypes);
-    }
+
 }
