e4348fb09c99bbe38b2334e7a1517bd0faa89db5
Maxim <msirotkin95@gmail.com>
2023-10-28 16:50:52 +0300

    1st fix

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -1,30 +1,23 @@
 package hexlet.code;
 
-import hexlet.code.games.Even;
-import hexlet.code.games.Cli;
-import hexlet.code.games.Calc;
-import hexlet.code.games.GSD;
-import hexlet.code.games.Progression;
-import hexlet.code.games.Prime;
-
 import java.util.Scanner;
 
 public class Engine {
     public static void engine(String question, String answer, String name, int round, int roundQuantity) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println(question);
-            System.out.print("Your answer: ");
-            var userAnswer = scanner.nextLine();
-            if (userAnswer.equals(answer)) {
-                System.out.println("Correct!");
-                if (round == roundQuantity - 1) {
-                    System.out.println("Congratulations, " + name + "!");
-                }
-            } else {
-                System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '"
-                        + answer + "'");
-                System.out.println("Let's try again, " + name + "!");
-                System.exit(0);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(question);
+        System.out.print("Your answer: ");
+        var userAnswer = scanner.nextLine();
+        if (userAnswer.equals(answer)) {
+            System.out.println("Correct!");
+            if (round == roundQuantity - 1) {
+                System.out.println("Congratulations, " + name + "!");
+            }
+        } else {
+            System.out.println("'" + userAnswer + "' is wrong answer ;(. Correct answer was '"
+                    + answer + "'");
+            System.out.println("Let's try again, " + name + "!");
+            System.exit(0);
 
         }
     }
